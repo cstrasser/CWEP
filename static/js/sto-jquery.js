5adde39c684b290e2d244e97add8e0c1daad38fe
cstrasser <cstrasser11@gmail.com>
2015-06-05 15:30:27 -0400

    jquery search filter working

--- static/js/sto-jquery.js
+++ static/js/sto-jquery.js
@@ -1,11 +1,64 @@
 $(document).ready(function() {
- 
- $("#status").on('click', 'li', function(){
-    var stat =$(this).text();
-    //alert(stat );
-    //$.get('/sto/listview/',  function(stat){
+
+ $("#rental_filter").on('click',function(){
+ // $(".stoitem, td").hide();
+   //$( ".stoitem, td" ).each(function( i ) {
+    $( "#billtype" ).each(function( i ) {
+    //if ( this.style.color !== "green" ) {
+    if (this.text == "Rental" ) {
+      this.style.color = "green";
+      //alert(this.text);
+    } else {
+      this.style.color = "blue";
+      }
+  });  
+});
+    
    
-});           
+    //$.get('/sto/listview/',  function(stat){  
+ 
+ 
+ $("#warranty_filter").on('click',function(){
+    //default each row to visible
+     $('.stoitem, td').show();
+     
+    var $rowsNo = $('#mytable tbody tr').filter(function () {
+        return $.trim($(this).find('td').eq(7).text()) !== "Warranty"}).toggle();
+
+   
+});
+ 
+    
+ $("#bill_filter").on('click',function(){
+    $(".stoitem, td").hide()    
+});
+ 
+ 
+  
       
-    });
+            
+    }); //doc ready 
+
+//http://stackoverflow.com/questions/15688147/hide-row-in-table-based-on-td-cell-in-another-row-using-jquery
+//https://api.jquery.com/category/traversing/filtering/
+// }).parent('tr').css('color','red');
+
+//var code = e.keyCode || e.which;
+// if(code == 13) { //Enter keycode
+//   //Do something
+// }
+
+//http://stackoverflow.com/questions/12385343/jquery-show-hide-table-rows-based-on-cell-content   
+     
+      
+      //alert($(".stoitem").parent('a').val);
+     // $( "li" ).not( ":even" ).css( "background-color", "red" );
+     // $( ".stoitem, tr:contains('201')" ).show('tr');
+      //$( "td:contains('20190')" ).show();
+
+
+
+
+
+
 
